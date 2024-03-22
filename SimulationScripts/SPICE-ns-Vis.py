57dd9b504f0a7e7b5811d8d43b7c369643d53d1b
Jason Pandian <100123063+PandiaJason@users.noreply.github.com>
2024-03-22 12:42:46 +0530

    Update SPICE-ns-Vis.py

--- SimulationScripts/SPICE-ns-Vis.py
+++ SimulationScripts/SPICE-ns-Vis.py
@@ -5,11 +5,16 @@ from mpl_toolkits.mplot3d import Axes3D
 
 if __name__ == "__main__":
     spDict = {
-        "node=0": {"name": "MERCURY", "color": "green"},
-        "node=1": {"name": "VENUS", "color": "blue"},
-        "node=2": {"name": "EARTH", "color": "brown"},
-        "node=3": {"name": "MARS", "color": "orange"},
-        "node=4": {"name": "JUPITER", "color": "black"}
+        "node=0": {"name": "MERCURY", "color": "orange"},
+        "node=1": {"name": "VENUS", "color": "brown"},
+        "node=2": {"name": "EARTH", "color": "blue"},
+        "node=3": {"name": "MARS", "color": "darkred"},
+        "node=4": {"name": "JUPITER", "color": "gold"},
+        "node=5": {"name": "PLUTO", "color": "darkmagenta"}
+        # "node=6": {"name": "URANUS", "color": "red"},
+        # "node=7": {"name": "NEPTUNE", "color": "black"},
+        # "node=8": {"name": "PLUTO", "color": "brown"}
+
     }
     
     # Create a single figure and subplot outside the loop
@@ -39,8 +44,8 @@ if __name__ == "__main__":
         ax.plot(X, Y, Z, color=attributes['color'], label=attributes['name'])
 
     # Move legend, title, and supertitle outside the loop to avoid duplication
-    plt.legend(loc='lower left', bbox_to_anchor=(-0.4, -0.1), fontsize='small', title='Spacecrafts')
-    plt.title('Visualizing SPICE-ns-3 Mobility Traces of Planets', fontsize=16)
-    plt.suptitle('J2000 Reference Frame - Duration : June 06, 1998 to Dec 12, 1999', fontsize=12)
+    plt.legend(loc='lower left', bbox_to_anchor=(-0.4, -0.1), fontsize='small', title='PLANETS')
+    plt.title('Visualizing Planets Mobility with SPICE-ns under ns-3 Traces', fontsize=16)
+    plt.suptitle('J2000 Reference Frame - Duration : June 06, 1998 to Dec 12, 2023', fontsize=12)
 
     plt.show()
